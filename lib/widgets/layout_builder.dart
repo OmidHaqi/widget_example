1fae9f513cec7e524d97537ff738e70abd3cc651
Umut <omid.haqi@outlook.com>
2024-03-31 20:42:51 +0330

    add tablet constraints

--- lib/widgets/layout_builder.dart
+++ lib/widgets/layout_builder.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
 
 class LayOutBuilder extends StatelessWidget {
+  const LayOutBuilder({super.key});
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-      if (constraints.maxWidth < 600) {
-        return SafeArea(
+      if (constraints.maxWidth<650 ) {
+        return const SafeArea(
           child: Scaffold(
             body: Center(
-              child: Text("Hello I\'m using LayoutBuilder maxWidth < 600 for show in mobile or table"),
+              child: Text("Mobile"),
             ),
           ),
         );
-      } else {
-        return SafeArea(
+      } else if(constraints.maxWidth <1100 && constraints.maxWidth>=650) {
+        return const SafeArea(
           child: Scaffold(
             body: Center(
-              child: Text("Hello I\'m using LayoutBuilder maxWidth > 600 for show in web"),
+              child: Text("Tablet"),
+            ),
+          ),
+        );
+      }else{
+        return const SafeArea(
+          child: Scaffold(
+            body: Center(
+              child: Text("Desktop"),
             ),
           ),
         );
